7d00aca195ecd1d730bff861a60f5e9f9bc5fba0
fulldump <gerardooscarjt@gmail.com>
2025-03-08 04:55:38 +0100

    refactor: tests less verbose with apitest+biff

--- api/api_test.go
+++ api/api_test.go
@@ -1,58 +1,32 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"testing"
+
+	"github.com/fulldump/apitest"
+	"github.com/fulldump/biff"
 )
 
 func TestNewApi(t *testing.T) {
 
 	h := NewApi("testversion")
-	s := httptest.NewServer(h)
+	a := apitest.NewWithHandler(h)
 
 	t.Run("Request /version", func(t *testing.T) {
-		req, _ := http.NewRequest("GET", s.URL+"/version", nil)
+		r := a.Request("GET", "/version").Do()
 
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if res.StatusCode != 200 {
-			t.Fatalf("response status is %d, want 200", res.StatusCode)
-		}
-
-		body, _ := ioutil.ReadAll(res.Body)
-		if string(body) != "testversion" {
-			t.Fatalf("response body is %s, want %s", string(body), "testversion")
-		}
-
+		biff.AssertEqual(r.StatusCode, http.StatusOK)
+		biff.AssertEqual(r.BodyString(), "testversion")
 	})
 
 	t.Run("Request /hello", func(t *testing.T) {
-		payload := bytes.NewReader([]byte(`{"name": "Manu"}`))
-		req, _ := http.NewRequest("POST", s.URL+"/hello", payload)
+		r := a.Request("POST", "/hello").
+			WithBodyJson(map[string]string{"name": "Manu"}).
+			Do()
 
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if res.StatusCode != 200 {
-			t.Fatalf("response status is %d, want 200", res.StatusCode)
-		}
-
-		body := map[string]interface{}{}
-		json.NewDecoder(res.Body).Decode(&body)
-
-		have := body["message"]
-		want := "Hello Manu!"
-		if have != want {
-			t.Fatalf("response body is %s, want %s", have, want)
-		}
+		biff.AssertEqual(r.StatusCode, http.StatusOK)
+		biff.AssertEqual(r.BodyJsonMap()["message"], "Hello Manu!")
 	})
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/SeniorGo/seniorgocms
 
 go 1.24.1
+
+require (
+	github.com/fulldump/apitest v1.3.0
+	github.com/fulldump/biff v1.3.0
+)
