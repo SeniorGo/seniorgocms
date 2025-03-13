6138d8f1a1daeb3b996f831e51456a51c29a5417
fulldump <gerardooscarjt@gmail.com>
2025-03-14 00:19:04 +0100

    feat: login/logout buttons

--- statics/www/js/index.js
+++ statics/www/js/index.js
@@ -3,3 +3,19 @@ fetch("/version")
   .then((version) => {
     document.getElementById("version").innerText = version;
   });
+
+fetch("/auth/me")
+    .then((req) => req.json())
+    .then((payload) => {
+        if (payload.error) {
+            // No logged in
+            document.getElementById('auth_login').style.display = '';
+        } else {
+            // Logged in
+            console.log(payload);
+            console.log(payload.nick);
+            document.getElementById('auth_logout').style.display = '';
+            document.getElementById('auth_nick').style.display = '';
+            document.getElementById('auth_nick').innerText = payload.nick;
+        }
+    });
